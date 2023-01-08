840ce4abca99ceb0378e9a447a304063fdceac34
hoge <hoge@hogehoge.com>
2023-01-08 18:12:33 +0900

    tone sounder

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include "tb62709.h"
 #include "MsTimer2.h"
 
-
+#define TONE_PIN 9
 
 //return value = carriage
 bool count60(int *count){
@@ -23,14 +23,10 @@ bool count60(int *count){
 }
 
 
-int minuites[2];
-int seconds[2];
-int tick_count = 0;
 
+volatile bool tick_flag = false;
 void tick(void){
-    if(count60(seconds)){
-        count60(minuites);
-    }
+    tick_flag = true;
 }
 
 void setup(){
@@ -38,12 +34,36 @@ void setup(){
 
     MsTimer2::set(10, tick);
     MsTimer2::start();
+
+    pinMode(TONE_PIN,OUTPUT);
+}
+
+void tone_sound(){
+    tone(TONE_PIN,440,100);
+    delay(100);
+    tone(TONE_PIN,392,100);
+    delay(100);
+    digitalWrite(TONE_PIN,LOW);
 }
 
 
-
+int minuites[2];
+int seconds[2];
+int tick_count = 0;
 
 void loop(){
+
+    while(!tick_flag);
+    tick_flag = false;
+
+
+    if(count60(seconds)){
+        tone_sound();
+        if(count60(minuites)){
+            tone_sound();
+            tone_sound();
+        }
+    }
 
     unsigned char digits[4] = {0,0,0,0};
     digits[3] = minuites[1];
